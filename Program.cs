2634313cf1f8fd64b37ad251a0422d5f3e251751
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-05-21 17:51:57 +0200

    enabled Swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo
+  {
+    Title = "Pizza Api",
+    Description = "Making the pizza",
+    Version = "v1"
+  });
+})
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza API");
+});
 
 app.MapGet("/", () => "Hello World!");
 
